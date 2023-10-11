58dbdd1a4da6de1f5ba58ef0d7502a6a987fa3f0
jefry_nm <jefroy95@gmail.com>
2023-10-11 01:34:06 -0500

    Subiendo de nivel al colicionar con el regalito  _ Class 14

--- game.js
+++ game.js
@@ -10,6 +10,8 @@ const btnDown = $('#down');
 
 let canvasSize;
 let elementSize;
+// CLASS 14 : creamos la variable para el cambio de niveles 
+let level = 0;
 
 const playerPosition = {
   x: undefined,
@@ -21,6 +23,8 @@ const giftPosition = {
   x: undefined,
   y: undefined,
 };
+// Class 13 : Creando arreglo de enemigos
+  let enemyPositions = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -52,10 +56,19 @@ function startGame() {
   game.font = `${elementSize}px arial`;
   game.textAlign = '';
 
-  const map = maps[0 ];
+  const map = maps[level];
+
+  if (!map) {
+    gameWin();
+  }
+
   const mapRows = map.trim().split('\n');
   const mapRowCols = mapRows.map(row => row.trim().split('')); 
   console.log({map, mapRows, mapRowCols});
+
+// Class 13  Limpiamos nuestro arreglo  
+  enemyPositions = [];  
+
 //Nos ayuda a limpiar el mapa 
   game.clearRect(0,0,canvasSize, canvasSize);
 
@@ -77,7 +90,14 @@ function startGame() {
       }else if (col == 'I') {
         giftPosition.x = posX;
         giftPosition.y = posY;
+  // Ubicar la posición de la bombita y asigarla (Class 13 )
+      }else if (col == 'X') {
+        enemyPositions.push({
+          x: posX,
+          y: posY,
+        });
       }
+  //
 
 
       game.fillText(emoji, posX , posY)
@@ -94,19 +114,42 @@ function startGame() {
 };
 
 function movePlayer() {
-  const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3)
-  const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3)
+  const giftCollisionX = playerPosition.x.toFixed(2) == giftPosition.x.toFixed(2)
+  const giftCollisionY = playerPosition.y.toFixed(2) == giftPosition.y.toFixed(2)
   const giftCollision = giftCollisionX && giftCollisionY
 
   console.log({giftCollisionX, giftCollisionY});
 
+// Class 14 : Usamos la condicional para ejecutar la función LEVELWIN()
   if (giftCollision) {
-    console.log( 'Subiste de nivel!!!');
+    // console.log( 'Subiste de nivel!!!');
+    levelWin();
+  }
+// Class 13 Detectando colisiones con el método find
+  const enemyCollision = enemyPositions.find(enemy => {
+    const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+    const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+    return enemyCollisionX && enemyCollisionY;
+  });
+
+  if (enemyCollision) {
+    // console.log('Hubo colisió!!! :c');
   }
 
   game.fillText(emojis['PLAYER'],playerPosition.x, playerPosition.y);
     
-  }
+}
+
+function levelWin() {
+  console.log('Subiste de nivel!!');
+  level++;
+  startGame();
+}
+
+function gameWin() {
+  console.log('TERMINASTE EL JUEGO !!!');
+  return;
+}
 
 window.addEventListener('keydown', moveByKeys);
 btnUp.addEventListener('click', moveUp);
@@ -137,7 +180,7 @@ function moveByKeys(event) {
   
 // }
 function moveUp() {
-    if ((playerPosition.y - elementSize) < elementSize ) {
+    if ((playerPosition.y - elementSize) <= 0 ) {
         console.log("OUT");
     } else {
       playerPosition.y -= elementSize;
@@ -145,7 +188,7 @@ function moveUp() {
     }
 }
 function moveLeft() {
-    if ((playerPosition.x - elementSize) < elementSize ) {
+    if ((elementSize + playerPosition.x ) <= elementSize ) {
         console.log("OUT");
     } else {
       playerPosition.x  -= elementSize;
@@ -153,7 +196,7 @@ function moveLeft() {
     }
 }
 function moveRight() {
-    if ((playerPosition.x + elementSize) > canvasSize -elementSize ) {
+    if ((playerPosition.x + elementSize) >= canvasSize - elementSize ) {
         console.log("OUT");
     } else {
       playerPosition.x  += elementSize;
